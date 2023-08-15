b2df80fd4a0bbcb899382d28ea90c4082414d9ac
darya.tarasova <darya.tarasova@alteryx.com>
2023-08-15 17:01:53 +0200

    refactored

---
+++ src/models/model.js
@@ -0,0 +1,18 @@
+const { Contract } = require('./Contract');
+const { Profile } = require('./Profile');
+const { Job } = require('./Job');
+const { sequelize } = require('../config/db');
+
+Profile.hasMany(Contract, { as: 'Contractor', foreignKey: 'ContractorId' });
+Contract.belongsTo(Profile, { as: 'Contractor' });
+Profile.hasMany(Contract, { as: 'Client', foreignKey: 'ClientId' });
+Contract.belongsTo(Profile, { as: 'Client' });
+Contract.hasMany(Job);
+Job.belongsTo(Contract);
+
+module.exports = {
+  sequelize,
+  Profile,
+  Contract,
+  Job,
+};
--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ init();
 async function init() {
   try {
     app.listen(3001, () => {
+      /* eslint-disable-next-line no-console */
       console.log('Express App Listening on Port 3001');
     });
   } catch (error) {
+    /* eslint-disable-next-line no-console */
     console.error(`An error occurred: ${JSON.stringify(error)}`);
     process.exit(1);
   }
